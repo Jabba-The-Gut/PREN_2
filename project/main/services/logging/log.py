124abc7792cb093c84e68df3a7a409ec1f6390c9
David Gut <gud@iflow.ch>
2020-04-21 20:08:22 +0200

    Adapted to structure changes

---
+++ project/main/services/logging/log.py
@@ -0,0 +1,41 @@
+import pika
+from datetime import datetime
+from main.const import const
+
+
+def _run():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=const.CONNECTION_STRING))
+    channel = connection.channel()
+    channel.exchange_declare(exchange=const.EXCHANGE, exchange_type='topic')
+
+    result = channel.queue_declare(const.LOG_QUEUE_NAME, exclusive=False)
+
+    channel.queue_bind(
+        exchange='main', queue=const.LOG_QUEUE_NAME, routing_key=const.LOG_BINDING_KEY
+    )
+
+    print(' [*] Waiting for logs. To exit press CTRL-C')
+
+    def callback(ch, method, properties, body):
+        print(" [x] %r:%r" % (method.routing_key, body))
+        # Will write to file the log message here
+        with open("logs.txt", "a+") as file:
+            file.seek(0)
+            data = file.read(100)
+            if len(data) > 0:
+                file.write("\n")
+            now = datetime.now()
+            current = now.strftime("%H:%M:%S")
+            to_write = str(current) + " " + str(body)
+            file.write(str(to_write))
+            file.close()
+
+    channel.basic_consume(
+        queue=const.LOG_QUEUE_NAME, on_message_callback=callback, auto_ack=True
+    )
+
+    channel.start_consuming()
+
+
+def main():
+    _run()
--- project/main/services/status/status.py
+++ project/main/services/status/status.py
@@ -1,47 +1,76 @@
 import pika
-import const
-
-# Variables that keep track of the various important statuses. (i.e. Memory, PX4, Sensor, System)
-__memory_ok = True
-__px4_running = True
-__sensor_ok = True
-__system_ok = True
+from main.const import const
 
 
-# mutator methods for other services to write to the above status flags
-def change_memory_status(new_status : bool) -> None:
-    global __memory_ok
-    __memory_ok = new_status
+def _run():
+    """
+    Contains the main logic for the status module
+    """
+    # Basic initialization of the rabbitMQ backend
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=const.CONNECTION_STRING))
+    channel = connection.channel()
+    channel.exchange_declare(exchange=const.EXCHANGE, exchange_type='topic')
+
+    result = channel.queue_declare(const.STATUS_QUEUE_NAME, exclusive=False)
+
+    binding_key = const.STATUS_BINDING_KEY
+
+    channel.queue_bind(
+        exchange='main', queue=const.STATUS_QUEUE_NAME, routing_key=const.STATUS_QUEUE_NAME
+    )
+
+    px4_running = False
+    system_ok = False
+
+    # Internal flags for this module to evaluate the system_ok flag
+
+    __logic_service = False
+    __data_processing_service = False
+    __logging_service = False
+
+    # Callback method
+    def evaluate_status_flags(ch, method, properties, body):
+        global px4_running, __data_processing_service, __logic_service, __logging_service, system_ok
+        system_ok = px4_running and __data_processing_service and __logging_service and __logic_service
+        if not system_ok:
+            channel.basic_publish(
+                exchange=const.EXCHANGE,
+                routing_key=const.LOGIC_QUEUE_NAME,
+                body="status: system_ok: {0}".format(system_ok)
+            )
+        if str(body).__contains__(const.STATUS_INIT_PX4_FLAG_TRUE):
+            print("PX4 is started and running")  # Log these in the future
+            px4_running = True
+        elif str(body).__contains__(const.STATUS_INIT_PX4_STATUS_FALSE):
+            print("PX4 did not start up successfully")  # Log these in the future
+            px4_running = False
+            channel.basic_publish(
+                exchange=const.EXCHANGE,
+                routing_key=const.DATA_PROCESSING_QUEUE_NAME,
+                body="status: __px4_running: {0}".format(px4_running)
+            )
+        elif str(body).__contains__(const.STATUS_COMMANDS_UNSUCCESSFUL):
+            px4_running = False
+            channel.basic_publish(
+                exchange=const.EXCHANGE,
+                routing_key=const.DATA_PROCESSING_QUEUE_NAME,
+                body="status: __px4_running: {0}".format(px4_running)
+            )
+        elif str(body).__contains__(const.STATUS_DATAPROC_MODULE_FLAG_TRUE):
+            __data_processing_service = True
+        elif str(body).__contains__(const.STATUS_DATAPROC_MODULE_FLAG_FALSE):
+            __data_processing_service = False
+            # Send a message here
+        else:
+            print("Default")
+
+    channel.basic_consume(
+        queue=const.STATUS_QUEUE_NAME, on_message_callback=evaluate_status_flags, auto_ack=True
+    )
+    print("Status Module running and waiting on messages to relay...")
+    channel.start_consuming()
+    connection.close()
 
 
-def change_px4_status (new_status : bool) -> None:
-    global __px4_running
-    __px4_running = new_status
-
-
-def change_sensor_status(new_status : bool) -> None:
-    global __sensor_ok
-    __sensor_ok = new_status
-
-
-def change_system_status(new_status : bool) -> None:
-    global __system_ok
-    __system_ok = new_status
-
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=const.CONNECTION_STRING))
-channel = connection.channel()
-channel.exchange_declare(exchange=const.EXCHANGE, exchange_type='topic')
-
-result = channel.queue_declare(const.STATUS_QUEUE_NAME, exclusive=False)
-
-binding_key = const.STATUS_BINDING_KEY
-
-channel.queue_bind(
-    exchange='main', queue=const.STATUS_QUEUE_NAME, routing_key=const.STATUS_BINDING_KEY
-)
-
-to_publish = "Memory Status {0}, PX4 Status {1}, Sensor Status {2}, System Status {3}".format(__memory_ok, __px4_running, __sensor_ok, __system_ok)
-channel.basic_publish(exchange=const.EXCHANGE, routing_key=const.STATUS_BINDING_KEY, body=to_publish)
-print(" [x] Sent %r" % to_publish)
-connection.close()
+def main():
+    _run()
