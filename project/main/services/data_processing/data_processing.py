c50d39dbd8a6cba120aac4b6a79c5f169838fa42
David Gut <gud@iflow.ch>
2020-04-23 10:50:40 +0200

    Corrected bug

--- project/main/services/data_processing/data_processing.py
+++ project/main/services/data_processing/data_processing.py
@@ -184,7 +184,7 @@ def main():
     # create consumer
     DataProcessingConsumer(service)
 
-    atexit.register(service.at_exit())
-    
+    atexit.register(service.at_exit)
+
     # start logic
     service.run()
